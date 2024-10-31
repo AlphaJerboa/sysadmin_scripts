61f1bb891e82cac7cc022f4cb250a01fb16dddf2
alphajerboa <github@jtrack.fr>
2024-10-31 08:49:12 +0100

    fix certificate expiration in openbao_ssh_client.sh

--- openbao_ssh_client.sh
+++ openbao_ssh_client.sh
@@ -20,28 +20,51 @@ CONF_INI="$SCRIPT_PATH/config.ini"
 [[ -f $CONF_INI ]] && . $CONF_INI
 
 # check settings definition
-[[ -z "$VAULT_ADDR" || -z "$ROLE" || -z "$PRINCIPAL" || -z "$SSH_PUB_FILE" || -z "$SSH_KEY_FILE" || -z "$1" ]] && cat << EOF && exit
+[[ -z "$VAULT_ADDR" || -z "$ROLE" || -z "$PRINCIPAL" || -z "$SSH_PUB_FILE" || -z "$SSH_KEY_FILE" || -z "$1" || -z "$LOGIN_METHOD" || -z "$LOGIN_USERNAME" ]] && cat << EOF && exit
 Usage: $0 <ssh_arguments>
 
 Following Parameters must be defined in $CONF_INI file
 VAULT_ADDR="https://<openbao_url>"
+LOGIN_METHOD=<userpass>
+LOGIN_USERNAME=<username to login to openbao>
 ROLE=<rolename>
 PRINCIPAL=<principalname>
 SSH_PUB_FILE=<path_to_public_ssh_key>
 SSH_KEY_FILE=<path_to_private_ssh_key>
 EOF
 
+export VAULT_CLIENT_TIMEOUT=2
+export VAULT_ADDR 
+
 # Record SSH arguments
 SSH_ARGS=$@
 
+# bao command line
+BAO_CMD="bao" 
+
+bao_login(){
+  $BAO_CMD login -method=$LOGIN_METHOD username=$LOGIN_USERNAME 
+}
+
+# Sign the public key with openbao ssh secrets engine
 sign_key(){
-  VAULT_ADDR=${VAULT_ADDR:-https://127.0.0.1} bao write -field=signed_key ssh/sign/${ROLE} public_key="$(cat ${SSH_PUB_FILE})" valid_principals=${PRINCIPAL} > $SIGNED_SSH_PUB_FILE
+  ! check_token && bao_login
+  $BAO_CMD write -field=signed_key ssh/sign/${ROLE} public_key="$(cat ${SSH_PUB_FILE})" valid_principals=${PRINCIPAL} > $SIGNED_SSH_PUB_FILE
   chmod 600 $SIGNED_SSH_PUB_FILE
   login_w_key
 }
 
+# Check if the signed key is not expired
+check_key_is_valid(){
+  EXPIRATION_DATE=$(ssh-keygen -L -f ${SIGNED_SSH_PUB_FILE} | sed -nE "/^[ ]*Valid: from/ s/^[ ]*Valid: from .* to (.*)$/\1/p");[[ $(date -d "$EXPIRATION_DATE" +%s) -gt $(date +%s) ]] && true || false
+}
+
+check_token(){
+  $BAO_CMD token lookup &>/dev/null
+}
+
 login_w_key(){
-  ssh -o PubkeyAuthentication=yes -i $SSH_KEY_FILE -o IdentitiesOnly=yes $SSH_ARGS
+  ssh -o PasswordAuthentication=no -o PubkeyAuthentication=yes -i $SSH_KEY_FILE -o IdentitiesOnly=yes $SSH_ARGS
 }
 
 # Check public file access
@@ -52,13 +75,15 @@ login_w_key(){
 if [[ -f $SIGNED_SSH_PUB_FILE ]]
 then
   # Check if signed key is expired
-  CREATION_DURATION=$(( $(date +%s) - $(stat -c %W $SIGNED_SSH_PUB_FILE) ))
-  if [[ $CREATION_DURATION > $KEY_DURATION ]]
+  if check_key_is_valid
   then
+    # Use the existing signed to login
     login_w_key
   else
+    # Request to sign the provided public key
     sign_key
   fi
 else
+    # Request to sign the provided public key
     sign_key
 fi
